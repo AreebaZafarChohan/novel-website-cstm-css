11f67b8989fccc7597ff205b0a43977e2b21b618
AreebaZafarChohan <areebazafar715@gmail.com>
2024-09-27 22:33:30 +0500

    some changes

--- src/app/components/ContactSection.tsx
+++ src/app/components/ContactSection.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-
 const ContactSection: React.FC = () => {
   return (
     <div className="contact-container">
--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import HeroSecCard from "./HeroSecCard";
-import styles from "../../../styles/custom.module.css"; // Import custom CSS
 
 const HeroSection = () => {
   return (
